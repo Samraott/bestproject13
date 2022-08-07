1bc7a49836d188d661cc611490e3f3b46110e5fe
Katrinabot <105980103+Katrinabot@users.noreply.github.com>
2022-08-07 11:51:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -191,7 +191,7 @@ def get_size(size):
     while size >= 1024.0 and i < len(units):
         i += 1
         size /= 1024.0
-    return "%.4f %s" % (size, units[i])
+    return "%.2f %s" % (size, units[i])
 
 def split_list(l, n):
     for i in range(0, len(l), n):
